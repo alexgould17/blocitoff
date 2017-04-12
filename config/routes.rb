811d78f357db73dc47c638b9c1f8da8450a8945b
Alex Gould <alexgould17@gmail.com>
2017-04-11 22:46:09 -0700

    Added barebones home controller + routes, basic navbar & incorporated bootstrap into app scss & js files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # Route root path thru the home controller to the index page
+  root 'home#index'
+
+  # Other HomeController routes
+  get 'about' => 'home#about'
+  get 'faq' => 'home#faq'
+  get 'contact' => 'home#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
