805e44604de6177c40f28096e5b9b484b8a5b8b5
Alex Gould <alexgould17@gmail.com>
2017-04-12 21:32:50 -0700

    Added profile view (barebones currently, users have no info besides name & created_at rn) & made it so devise redirects to the profile after sign in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+	
+	before_save do
+		self.email.downcase!
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Route root path thru the home controller to the index page
   root 'home#index'
 
@@ -7,6 +6,10 @@ Rails.application.routes.draw do
   get 'about' => 'home#about'
   get 'faq' => 'home#faq'
   get 'contact' => 'home#contact'
+	
+	# User routes. Show separate because devise only handles login, registration, etc., not profile viewing.
+	devise_for :users, controllers: { sessions: 'users/sessions' }
+	resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
