64df3875b90fe384e3645040824219bed7561c5d
Alex Gould <alexgould17@gmail.com>
2017-07-19 22:00:08 -0700

    Minor changes to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 # Our standard test password & array of users (we'll need this to bypass devise's confirm step in a bit).
 test_users = []
 pw = "wsxedc"
-item_count = 25
 
 # Create my usual test admin account
-admin_acct = User.create!(name: "Alex Gould", email: "alexgould17@gmail.com", password: pw)
-test_users << admin_acct
+me = User.find_or_create_by(email: 'alexgould17@gmail.com') do |me|
+  me.name = "Alex Gould"
+  me.password = pw
+end
+test_users << me
 
 # The rest of the seeding we do unless we're in production
 unless Rails.env.production?
@@ -17,9 +19,9 @@ unless Rails.env.production?
 			password: pw
 		)
 	end
-	
+
 	# Create 25 items randomly assigned to a user in our test_users bunch
-	item_count.times do
+	25.times do
 		Item.create!(
 			name: Faker::Beer.unique.name,
 			user: test_users.sample
@@ -31,4 +33,4 @@ end
 test_users.each { |u| u.update_attribute(:confirmed_at, 1.second.ago) }
 
 # Log our results
-puts "Seeded the database with #{test_users.count} users & #{item_count} items."
+puts "Seeded the database with #{test_users.count} users & #{Item.count} items."
