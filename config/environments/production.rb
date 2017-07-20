0bbc7176fd7cb5222069cfcf4947cd07a55d1d1b
Alex Gould <alexgould17@gmail.com>
2017-07-19 21:54:10 -0700

    Updated email server to reflect new Heroku app name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-	
+
 	# Use Heroku's mailer for the mailer in production
-	config.action_mailer.default_url_options = { host: 'powerful-reef-31276.herokuapp.com' }
+	config.action_mailer.default_url_options = { host: 'ag17-blocitoff.herokuapp.com' }
 end
