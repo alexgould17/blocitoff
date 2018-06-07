aaf58e3df223cf3e02b2ed9db556fc78c9e3ce1d
Alex Gould <alexgould17@gmail.com>
2018-06-06 20:52:10 -0700

    Updated seeds.rb, changed admin account to email:admin@test.test / pw:wsxedc. Also seeds sample items to production now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
 # Our standard test password & array of users (we'll need this to bypass devise's confirm step in a bit).
 test_users = []
 pw = "wsxedc"
+admin_email = "admin@test.test"
 
 # Create my usual test admin account
-me = User.find_or_create_by(email: 'alexgould17@gmail.com') do |me|
-  me.name = "Alex Gould"
+me = User.find_or_create_by(email: admin_email) do |me|
+  me.name = "Test Admin"
   me.password = pw
 end
 test_users << me
 
-# The rest of the seeding we do unless we're in production
+# Don't seed test users unless we're in production
 unless Rails.env.production?
 	# Create 5 test users
 	5.times do
@@ -19,15 +20,15 @@ unless Rails.env.production?
 			password: pw
 		)
 	end
+end
 
-	# Create 25 items randomly assigned to a user in our test_users bunch
+# Create 25 items randomly assigned to a user in our test_users bunch
 	25.times do
 		Item.create!(
 			name: Faker::Beer.unique.name,
 			user: test_users.sample
 		)
 	end
-end
 
 # Bypass devise's confirm step
 test_users.each { |u| u.update_attribute(:confirmed_at, 1.second.ago) }
